0dcc48f7227174e59e2440c0f475def30d82becb
Jason Vanderhoof <jvanderhoof@gmail.com>
2012-01-05 15:41:53 -0500

    added constants, and page layout for editing

--- lib/my_controller_patch.rb
+++ lib/my_controller_patch.rb
@@ -4,13 +4,37 @@ module MyControllerPatch
 
     base.class_eval do
       alias_method_chain :page, :custom_page
+      alias_method_chain :page_layout, :custom_page_layout
     end
   end
 
   module InstanceMethods
+    
+    BLOCKS = { 'issuesassignedtome' => :label_assigned_to_me_issues,
+               'issuesreportedbyme' => :label_reported_issues,
+               'issueswatched' => :label_watched_issues,
+               'news' => :label_news_latest,
+               'calendar' => :label_calendar,
+               'documents' => :label_document_plural,
+               'timelog' => :label_spent_time
+             }.merge(Redmine::Views::MyPage::Block.additional_blocks).freeze
+    
+    DEFAULT_LAYOUT = {  'left' => ['issuesassignedtome'], 
+                       'right' => ['issuesreportedbyme'] 
+                    }.freeze
+    
+
+    def page_layout_with_custom_page_layout
+      @year ||= Date.today.year
+      @week ||= Date.today.cweek
+      @user = User.current
+      @blocks = @user.pref[:my_page_layout] || DEFAULT_LAYOUT.dup
+      @block_options = []
+      BLOCKS.each {|k, v| @block_options << [l("my.blocks.#{v}", :default => [v, v.to_s.humanize]), k.dasherize]}
+    end
+    
+    
     def page_with_custom_page
-      Rails.logger.info "page_with_custom_page (params): #{params.inspect}"      
-      #raise params[:week].to_i.class.to_s
       if params[:year] and params[:year].to_i > 1900
         @year = params[:year].to_i
         if params[:week] and params[:week].to_i > 0 and params[:week].to_i < 53
